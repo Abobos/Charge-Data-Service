4faa50f9acbf9c04b3d9df0594e5798388233848
abobos <blessingmakaraba@gmail.com>
2023-05-08 01:00:16 +0200

    chore(docs): Adds documentation

--- apps/task-service/src/api.service.ts
+++ apps/task-service/src/api.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { configuration } from './config';
+import { configuration } from '../../config';
 import { IOpenMapChargeData } from '@charge-data/dal';
 
 @Injectable()
